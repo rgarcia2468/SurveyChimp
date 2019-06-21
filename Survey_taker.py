e82e30587b17d0e59a15c2c53f388345ce753476
Rodrigo Garcia <rodrigogarcia2468@outlook.com>
2019-06-21 14:15:04 -0700

    This is the second draft for both Survey_taker and function.

--- Survey_taker.py
+++ Survey_taker.py
@@ -1,15 +1,14 @@
 import json
-survey= open("finished_survey.txt", "r+")
-a= survey.read()
-b = json.loads(a)
-dict = {}
-for x in b:
-    questions= x.split(',')
+with open("finished_survey.txt", "r+") as a:
+    data = json.load(a)
+final = {}
+
+for x in data:
+    questions = x.split(',')
     print(str(questions[0]))
-    for y in questions:
-        direct= input("Write your answer to the Question below: ")
-        dict[y]= direct
+    direct= input("Write your answer to the question here: ")
+    final[x]= direct
 
 with open('Answered_survey.txt','w') as json_file:
-     json.dump(dict, json_file)
+     json.dump(final, json_file)
 print("Thank you for taking your time and answering this survey.")
--- function.py
+++ function.py
@@ -1,47 +1,28 @@
 
 import json
-def questions():
-    #counter= 0
-    #dict =
-    survey= []
+
+def there_is_another():
     while True:
+        ask_again= input("Would you like to ask another question? Please input either 'Yes' or 'No': ")
+        ask_again= ask_again.lower()
+        if ask_again== "yes":
+            return True
+        elif ask_again=="no":
+            return False
 
-        a=input("What question would you like to ask ?: ")
-        b= input("Would you like to ask another question? Input 'Yes' or'No': ")
-        #counter+=1
-        #dict[counter]= a
+
+def questions():
+    survey=[]
+    ask_more= True
+    while ask_more:
+        a=input("What question would you like to ask?: ")
         survey.append(a)
-
-
-        if b== "Yes":
-            pass
-
-        elif b=="No":
-            print("Your survey is now complete.")
-            break
-        else:
-            while True:
-                if b!= "No" or b!= "Yes":
-                    come_on=input("Please input either 'Yes' or 'No': ")
-                    if come_on == "Yes":
-                        break
-                    elif come_on=="No":
-                        print("Your survey is now complete.")
-                        break
-                    else:
-                        pass
-            if come_on== "Yes":
-                pass
-            else:
-                break
-
-
-
-
-
+        ask_more= there_is_another()
 
     with open('finished_survey.txt','w') as json_file:
-         json.dump(survey, json_file)
+        json.dump(survey, json_file)
+
+
 
 if __name__=='__main__':
     questions()
