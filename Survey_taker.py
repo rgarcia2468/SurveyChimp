ad0177699122f9ed6e3fe71d6e9a37a6e69423b2
Rodrigo Garcia <rodrigogarcia2468@outlook.com>
2019-06-24 20:45:01 -0700

    Draft 3 of Survey Chimp (Added multiple choice)

--- Survey_taker.py
+++ Survey_taker.py
@@ -5,9 +5,18 @@ final = {}
 
 for x in data:
     questions = x.split(',')
-    print(str(questions[0]))
-    direct= input("Write your answer to the question here: ")
-    final[x]= direct
+    for y in questions:
+        if y[0] == "O":
+            print(str(y[1:]))
+            direct= input("Write your answer to the question here: ")
+            final[y[1:]]= direct
+        else:
+            question = y[1:]
+            question= question.split("_")
+            #top = the question, ans1 = answer choice 1, ans2= answer choice2 ...
+            print("{top}\n A.{ans1}\n B.{ans2}\n C.{ans3}\n D.{ans4}".format(top =question[0],ans1= question[1],ans2= question[2],ans3= question[3],ans4=question[4]))
+            multiple_choice= input("Enter your Answer choice here:")
+            final[question[0]]=multiple_choice
 
 with open('Answered_survey.txt','w') as json_file:
      json.dump(final, json_file)
--- function.py
+++ function.py
@@ -1,6 +1,5 @@
-
 import json
-
+survey= []
 def there_is_another():
     while True:
         ask_again= input("Would you like to ask another question? Please input either 'Yes' or 'No': ")
@@ -9,19 +8,49 @@ def there_is_another():
             return True
         elif ask_again=="no":
             return False
+def multiple_choice():
+
+    q1= input("Enter Your question: ")
+    mca= input("Enter answer choice A: ")
+    mcb = input("Enter answer choice B: ")
+    mcc = input("Enter answer choice C: ")
+    mcd = input("Enter answer choice D: ")
+    all_of_it= 'M' + q1+"_"+mca+ "_"+mcb+ "_"+ mcc+"_"+mcd
+    survey.append(all_of_it)
+    with open('finished_survey.txt','w') as json_file:
+        json.dump(survey, json_file)
 
 
-def questions():
-    survey=[]
-    ask_more= True
-    while ask_more:
-        a=input("What question would you like to ask?: ")
-        survey.append(a)
-        ask_more= there_is_another()
+
+def open_ended():
+
+
+    a="O" + input("What question would you like to ask? : ")
+    survey.append(a)
+    #ask_more= there_is_another()
 
     with open('finished_survey.txt','w') as json_file:
         json.dump(survey, json_file)
 
+def which_one():
+    while True:
+        question= input("What type of question would you like to ask? Input 'Open ended' or 'Multiple choice': " )
+        question = question.lower()
+        if question== "multiple choice":
+            one = multiple_choice()
+            break
+        elif question== "open ended":
+            two= open_ended()
+            break
+        else:
+            pass
+
+def questions():
+    ask_more= True
+    while ask_more:
+        start= which_one()
+        ask_more = there_is_another()
+    print("Your survey is now complete.")
 
 
 if __name__=='__main__':
